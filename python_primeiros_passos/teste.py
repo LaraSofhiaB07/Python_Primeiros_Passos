00615922023e3739e554a15afde65bf7f26250bc
LaraSofhiaB07 <larabatista280@gmail.com>
2025-12-16 20:25:25 -0300

    First commit

---
+++ python_primeiros_passos/teste.py
@@ -0,0 +1,10 @@
+nome = input("Qual Seu nome? ")
+idade = int(input("Qual a sua idade? "))
+
+if idade >= 18:
+    print("Você é de maior")
+else:
+    print("Você é de menor")
+
+print("Hello Word")
+
