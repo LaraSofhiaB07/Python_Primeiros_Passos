136a4b4f4fa1e584baa079b95c2448aea41e3c08
LaraSofhiaB07 <larabatista280@gmail.com>
2025-12-16 20:34:24 -0300

    Verificando IMC

--- python_primeiros_passos/teste.py
+++ python_primeiros_passos/teste.py
@@ -1,10 +1,17 @@
 nome = input("Qual Seu nome? ")
 idade = int(input("Qual a sua idade? "))
+altura = float(input("Qual a sua altura?"))
+peso = float(input("Qual seu peso? "))
+imc = peso/altura
 
 if idade >= 18:
     print("Você é de maior")
 else:
     print("Você é de menor")
 
-print("Hello Word")
+if imc < 18.5:
+    print(f"Seu peso é {imc} e está Abaixo do peso")
+
+elif imc > 18.5 < 24.9:
+    print(f"Seu peso é {imc} e está normal")
 
