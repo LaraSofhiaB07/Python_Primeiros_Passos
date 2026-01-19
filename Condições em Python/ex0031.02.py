b9bcf42990aaa2455e1b02f334168bafd6319c02
LaraSofhiaB07 <larabatista280@gmail.com>
2026-01-18 22:29:36 -0300

    Custo da Viagem 2

---
+++ Condições em Python/ex0031.02.py
@@ -0,0 +1,8 @@
+print('Qual é a distância da sua viagem?: ')
+distancia = float(input())
+print('Você está prestes a começar uma viagem de {}Km.'.format(distancia))
+if distancia <= 200:
+    preço = distancia * 0.50
+else:
+    preço = distancia * 0.45
+print('E o preço da sua passagem será de R${:.2f}'.format(preço))
