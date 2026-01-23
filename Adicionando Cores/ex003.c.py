7eab03f8b7686e2dd3434e4a10342917490ebf67
LaraSofhiaB07 <larabatista280@gmail.com>
2026-01-22 22:11:07 -0300

    Soma entre Cor

---
+++ Adicionando Cores/ex003.c.py
@@ -0,0 +1,4 @@
+n1=int(input('\033[4;36;44mDigite um número:\033[m'))
+n2=int(input('\033[4;35;41mDigite mais um número:\033[m'))
+s=n1+n2
+print("\033[4;31;45mA soma entre {} e {} vale {}\033[m".format(n1, n2,s))
