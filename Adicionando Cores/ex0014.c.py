1274e77d38e70615ae6de6fd8bd6a78965fcfcd1
LaraSofhiaB07 <larabatista280@gmail.com>
2026-01-28 19:09:39 -0300

    teste

--- Adicionando Cores/ex0014.c.py
+++ Adicionando Cores/ex0014.c.py
@@ -1,3 +1,3 @@
 c = float(input('\033[31mInforme a temperatura em °C:\033[m'))
-f = 9 * c / 5 + 32
+f = 9 * c / 5 + 31
 print ('\033[32mA temperatura de \033[m\033[36m{}°C\033[m\033[32m corresponde a \033[m\033[36m{} °F!\033[m'.format(c, f))
