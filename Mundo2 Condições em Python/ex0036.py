502fca07266545a6988d6b2b83c30d28fa832a98
LaraSofhiaB07 <larabatista280@gmail.com>
2026-03-08 06:42:34 -0300

    iniciando

--- Mundo2 Condições em Python/ex0036.py
+++ Mundo2 Condições em Python/ex0036.py
@@ -1,11 +1,11 @@
-casa = float(input('\033[32mValor da casa: R$ \033[m'))
-salario = float(input('\033[35mSalário do comprador: R$ \033[m'))
-anos = int(input('\033[33mQuantos anos de financiamento? \033[m'))
+casa = float(input('Valor da casa: R$'))
+salario = float(input('Salário do comprador: R$ '))
+anos = int(input('Quantos anos de financiamento? '))
 prestacao = casa / (anos * 12)
 minimo = salario * 30 / 100
-print('\033[36mPara pagar uma casa de R$ {:.2f} em {} anos,\033[m'.format(casa, anos))
-print('\033[36ma prestação será de R$ {:.2f}\033[m'.format(prestacao))
+print('Para pagar uma casa de R$ {:.2f} em {} anos,'.format(casa, anos))
+print('a prestação será de R$ {:.2f}'.format(prestacao))
 if prestacao <= minimo:
-    print('\033[32mEmpréstimo pode ser CONCEDIDO!\033[m')
+    print('Empréstimo pode ser CONCEDIDO!')
 else:
-    print('\033[31mEmpréstimo NEGADO!\033[m')
+    print('Empréstimo NEGADO!')
